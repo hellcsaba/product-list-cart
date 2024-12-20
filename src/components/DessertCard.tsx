ab0cd3149f53d85aa2bc2a2c7ae22aee145223bd
Csaba Hell <ifj.hellcsaba@gmail.com>
2024-12-20 23:40:04 +0100

    Add BEM notated css class names and quantity button

--- src/components/DessertCard.tsx
+++ src/components/DessertCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 interface ImageProps {
   thumbnail: string;
@@ -16,18 +16,41 @@ interface DessertCardProps {
 }
 
 const DessertCard: React.FC<DessertCardProps> = ({ name, category, price, image }) => {
+  const [quantity, setQuantity] = useState<number>(0);
+  const handleAddToCart = () => setQuantity(1);
+  const handleIncrement = () => setQuantity((prev) => prev + 1);
+  const handleDecrement = () => setQuantity((prev) => (prev > 1 ? prev - 1 : 0));
+
   return (
-    <article>
-      <picture>
+    <article className='dessert'>
+      <picture className='dessert__picture'>
         <source srcSet={image.desktop} media="(min-width: 64rem)" />
         <source srcSet={image.tablet} media="(min-width: 48rem)" />
         <source srcSet={image.mobile} media="(min-width: 30rem)" />
         <img src={image.thumbnail} alt={name} />
       </picture>
-      <button>Add to Cart</button>
-      <p>{category}</p>
-      <h3>{name}</h3>
-      <p>${price}</p>
+      {quantity === 0 ? (
+        <button className='dessert__buy-button dessert__button--cart' onClick={handleAddToCart}><img src="/assets/images/icon-add-to-cart.svg" alt="Cart icon"/>Add to Cart</button>
+      ) : (
+        <div className="dessert__quantity-selector">
+          <button
+            className="dessert__quantity-button dessert__quantity-button--decrement"
+            onClick={handleDecrement}>
+            <img src="/assets/images/icon-decrement-quantity.svg" className="dessert__button-decrement" 
+            alt="Decrease quantity"/>
+          </button>
+          <span className="dessert__quantity">{quantity}</span>
+          <button
+            className="dessert__quantity-button dessert__quantity-button--increment"
+            onClick={handleIncrement}>
+            <img src="/assets/images/icon-increment-quantity.svg" className="dessert__button--increment" 
+            alt="Increase quantity"/>
+          </button>
+        </div>
+      )}
+      <p className='dessert__category'>{category}</p>
+      <h3 className='dessert__name'>{name}</h3>
+      <p className='dessert__price'>${price}</p>
     </article>
   )
 };
